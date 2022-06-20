1fd9a32dbac67418c9d234d2e11cb0a79393b2e6
Catalystiq <73408095+Catalystiq@users.noreply.github.com>
2022-06-19 21:49:42 -0600

    added basic image generation
    added basic image generation for the colors

--- src/generator.js
+++ src/generator.js
@@ -1,1 +1,65 @@
-console.log("Hello World!")
+const { readFileSync, writeFileSync, readdirSync, rmSync, existsSync, mkdirSync } = require('fs')
+const sharp = require('sharp')
+//const mongoose = require('mongoose')
+require('dotenv').config()
+let idx = 0;
+
+// mongoose.connect(process.env.DB_URI, {
+//     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+// })
+
+// const colorSchema = new mongoose.Schema({
+//     color: {
+//         type: String,
+//         required: true
+//     }
+// }, {timestamps: true})
+
+// const Color = mongoose.model('colors', colorSchema)
+
+function createImage(idx){
+    const template = `
+        <svg width="256" height="256" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="256" height="256" fill="rgb(0,0,0)"/>
+        </svg>
+    `
+
+    const meta = {
+        name: `rgb(0,0,0)`,
+        description: `rgb(0,0,0)`,
+        image: `${idx}.png`,
+        attributes: [
+            { }
+        ]
+    }
+    writeFileSync(`./out/${idx}.json`, JSON.stringify(meta))
+    writeFileSync(`./out/${idx}.svg`, template)
+    //svgToPng(idx)
+}
+
+
+// Create dir if not exists
+if (!existsSync('./out')){
+    mkdirSync('./out');
+}
+
+// Cleanup dir before each run
+readdirSync('./out').forEach(f => rmSync(`./out/${f}`));
+
+
+do {
+    createImage(idx);
+    idx--;
+  } while (idx >= 0);
+
+
+
+
+
+
+
+
+// function rgb(r, g, b){
+//     return `rgb(${r}, ${g}, ${b})`
+// }
+
